e3052746df33b50bee8a411ee2964938b78152cc
seonghyun <codiee@naver.com>
2025-08-26 01:59:28 +0900

    Feat: 네브 아이템스 변수 분리 및 tab-index 가능하게 Link로 변경하고 aria-label 달아줬음

--- app/(components)/nav/Nav.tsx
+++ app/(components)/nav/Nav.tsx
@@ -3,43 +3,32 @@
 import Image from "next/image";
 import React from "react";
 import logo from "@/public/icons/icon-logo-192x192.png";
-import { useRouter } from "next/navigation";
-import ThemeSwitcher from "./ThemeSwitcher";
+import ThemeSwitcher from "../theme/ThemeSwitcher";
+import Link from "next/link";
+import { navItemsList } from "./navItemsList";
 
 const Nav = () => {
-  const navItems = [
-    {
-      name: "홈",
-      link: "/",
-    },
-    { name: "게시물", link: "/board" },
-  ];
-  const router = useRouter();
   return (
-    <header className="w-full h-12 px-2 flex justify-between items-center border-b">
+    <header className="w-full h-12 px-2 flex justify-between items-center border-b dark:text-white">
       <div className="w-[70%] h-full flex">
         <div className="w-[5%] h-full flex justify-center items-center">
-          <Image
-            title="Noah 로고 [홈으로 이동하기]"
-            className="rounded-full cursor-pointer"
-            src={logo}
-            width={36}
-            height={36}
-            alt="Noah 홈페이지 로고"
-            priority
-          />
+          <Link href="/" aria-label="홈으로 이동하기" title="Noah 로고">
+            <Image
+              className="rounded-full cursor-pointer"
+              src={logo}
+              width={36}
+              height={36}
+              alt="Noah 홈페이지 로고"
+              priority
+            />
+          </Link>
         </div>
+
         <nav>
           <ul className="w-full h-full ml-6 flex justify-center items-center gap-4">
-            {navItems.map((item, idx) => (
-              <li
-                className="cursor-pointer"
-                onClick={() => {
-                  router.push(item.link);
-                }}
-                key={idx}
-              >
-                {item.name}
+            {navItemsList.map((item, idx) => (
+              <li key={idx}>
+                <a href={item.link}>{item.name}</a>
               </li>
             ))}
           </ul>
@@ -47,7 +36,11 @@ const Nav = () => {
       </div>
       <nav className="w-[30%] ">
         <ul className="h-full flex justify-end items-center gap-4">
-          <li>로그인</li>
+          <li>
+            <Link aria-label="로그인으로 이동하기" href="/signin">
+              로그인
+            </Link>
+          </li>
           <ThemeSwitcher />
         </ul>
       </nav>
