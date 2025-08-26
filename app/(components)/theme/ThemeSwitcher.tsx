2d3b5dddf4a9393049055edf8c6da53756fb1e7f
Hyun-EG <codiee@naver.com>
2025-08-26 16:18:33 +0900

    Feat: 버튼 type,aria-label 추가 및 아이콘 aria-label 삭제(중복 스크린 리드)

--- app/(components)/theme/ThemeSwitcher.tsx
+++ app/(components)/theme/ThemeSwitcher.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useTheme } from "next-themes";
 import { FaSun, FaMoon } from "react-icons/fa";
 import React from "react";
@@ -5,11 +7,16 @@ import React from "react";
 const ThemeSwitcher = () => {
   const { theme, setTheme } = useTheme();
   return (
-    <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+    <button
+      type="button"
+      aria-label="밝기 모드 변경하기"
+      className="flex items-center leading-none"
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
       {theme === "dark" ? (
-        <FaMoon aria-label="밝은 모드로 전환하기" fill="yellow" size={24} />
+        <FaMoon fill="yellow" size={24} />
       ) : (
-        <FaSun aria-label="다크 모드로 전환하기" size={24} />
+        <FaSun fill="orange" size={24} />
       )}
     </button>
   );
