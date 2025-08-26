09c4bc3d1644cb371e75d9f982cb177d7458629a
Hyun-EG <codiee@naver.com>
2025-08-26 19:03:29 +0900

    Style: 일반 투명도 점점 올리는 애니메이션이랑 투명도와 동시에 우측에서 오는 애니메이션은 공백이지만 일단 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ module.exports = {
   darkMode: "class",
   content: ["./app/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        // moveFadeIn: {
+        //   "0%": { opacity: "0", transform: "translateY(-40px)" },
+        //   "100%": { opacity: "1", transform: "translateY(0)" },
+        // },
+      },
+      animation: {
+        // moveFadeIn: "moveFadeIn 0.5s ease-in-out",
+        fadeIn: "fadeIn 0.5s ease-in-out",
+      },
+    },
   },
   plugins: [
     function ({ addUtilities }) {
