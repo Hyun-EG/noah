770f08cacdf2a6f6e99977b844724689c96a6892
Hyun-EG <codiee@naver.com>
2025-08-26 19:07:00 +0900

    Feat: 서브 메뉴 추가 및 토글 불필요 렌더 방지, 기본 레이아웃

--- app/(components)/nav/_components/SubNav.tsx
+++ app/(components)/nav/_components/SubNav.tsx
@@ -1,27 +1,58 @@
 "use client";
 
-import React, { useState } from "react";
-import { FiMenu } from "react-icons/fi";
+import React, { useCallback, useState } from "react";
+import { FiMenu, FiX } from "react-icons/fi";
 import { iconBtn } from "../variants";
+import { navItemsList } from "../navItemsList";
+import Link from "next/link";
 
 const SubNav = () => {
   const [isShowSubNav, setIsShowSubNav] = useState(false);
+
+  const toggleSubNav = useCallback(() => {
+    setIsShowSubNav((prev) => (prev ? false : true));
+  }, []);
+
   return (
     <>
       <button
-        onClick={() => {
-          setIsShowSubNav((prev) => (prev ? false : true));
-        }}
+        onClick={toggleSubNav}
         type="button"
         aria-label="메뉴 열기"
         className={iconBtn({ size: "md", subtle: true })}
       >
-        <FiMenu size={24} />
+        {isShowSubNav ? (
+          <FiX className="animate-fadeIn" size={24} />
+        ) : (
+          <FiMenu className="animate-fadeIn" size={24} />
+        )}
       </button>
       {isShowSubNav && (
-        <div className="absolute top-12 right-0 w-44 min-h-screen border-l bg-white dark:bg-black">
-          <div>sd</div>
-        </div>
+        <section className="fixed top-12 left-0 z-10 flex flex-col gap-4 w-full min-h-screen py-4 border-t border-sky-500 rounded-t-lg bg-white dark:bg-black dark:border-white animate-fadeIn">
+          <nav>
+            <ul className="flex flex-col gap-2 text-center">
+              <li className="mx-4 py-2 bg-sky-500 text-white rounded-xl dark:bg-white dark:text-black">
+                <Link href="/signin" aria-label="로그인으로 이동하기">
+                  로그인
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <nav>
+            <ul className="flex flex-col gap-2 text-center">
+              {navItemsList.map((item) => (
+                <li
+                  className="mx-4 py-2 border-b border-sky-500 dark:border-[#bebebe]"
+                  key={item.link}
+                >
+                  <Link href={item.link} aria-label={`${item.name}로 이동하기`}>
+                    {item.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </section>
       )}
     </>
   );
