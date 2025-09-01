9e59bb459c7a4a51739bd832ba04e276a325a047
Hyun-EG <codiee@naver.com>
2025-09-01 16:57:40 +0900

    Feat: 로그인 페이지 레이아웃 + 간단한 로그인 로직 구현

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,28 +1,15 @@
 import React from "react";
-import Input from "../(components)/common/Input";
+import SigninForm from "./_components/SigninForm";
 
-const LoginPage = () => {
+const SigninPage = () => {
   return (
     <section className="w-full px-4 flex flex-col items-center gap-4 justify-center h-screen text-black">
       <h1 className="text-xl text-center font-bold dark:text-white">
         관리자 로그인
       </h1>
-      <form className="w-full flex flex-col gap-4">
-        <Input name="id" type="text" placeholder="아이디를 입력해주세요." />
-        <Input
-          name="password"
-          type="password"
-          placeholder="비밀번호를 입력해주세요."
-        />
-        <button
-          className="h-16 bg-sky-500 rounded-lg text-white font-bold"
-          type="submit"
-        >
-          로그인
-        </button>
-      </form>
+      <SigninForm />
     </section>
   );
 };
 
-export default LoginPage;
+export default SigninPage;
