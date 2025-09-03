c4f89d02ad081ed9bac1737b5aa442f359e534b2
seonghyun <codiee@naver.com>
2025-09-04 00:31:26 +0900

    Feat: Nav 컴포넌트, Container 컴포넌트 전역 레이아웃에 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Nav } from "./(components)/nav/Nav";
+import Container from "./(components)/screen/Container";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,10 +39,11 @@ export default function RootLayout({
           as="style"
         />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} bg-white dark:bg-black`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Container>
+          <Nav />
+          {children}
+        </Container>
       </body>
     </html>
   );
