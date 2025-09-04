02cbd416ff4c500ec221bded63665357d2dfee21
Hyun-EG <codiee@naver.com>
2025-09-04 15:27:18 +0900

    Style: 기존 Variant 삭제, 테마명 변경

--- app/(components)/theme/ToggleTheme.tsx
+++ app/(components)/theme/ToggleTheme.tsx
@@ -2,7 +2,7 @@
 
 import { cva, VariantProps } from "class-variance-authority";
 import clsx from "clsx";
-import React from "react";
+import React, { useState } from "react";
 import { FaSun, FaMoon } from "react-icons/fa";
 
 interface ToggleThemeProps extends VariantProps<typeof toggleVariants> {
@@ -13,28 +13,26 @@ const toggleVariants = cva(
   "inline-flex items-center justify-center rounded-full p-2",
   {
     variants: {
-      theme: {
-        sun: {
-          backgroundColor: "#ffffff",
-        },
-        moon: {
-          backgroundColor: "#000000",
-        },
-      },
       size: {
         responsive: "h-8 w-8 sm:h-10 sm:w-10 md:h-12 md:w-12",
       },
       defaultVariants: {
-        theme: "sun",
+        themeMode: "sun",
         size: "responsive",
       },
     },
   }
 );
 
-const ToggleTheme = ({ className, theme, size }: ToggleThemeProps) => {
+const ToggleTheme = ({ className, size }: ToggleThemeProps) => {
+  const [theme, setTheme] = useState("sun");
   return (
-    <button className={clsx(toggleVariants({ theme, size }), className)}>
+    <button
+      onClick={() => {
+        setTheme(theme === "sun" ? "moon" : "sun");
+      }}
+      className={clsx(toggleVariants({ size }), className)}
+    >
       {theme === "sun" ? (
         <FaSun fill="orange" size={24} />
       ) : (
