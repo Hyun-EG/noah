8b32732cde18d81bc13e7fb0865c00fc37a111c7
Hyun-EG <codiee@naver.com>
2025-09-04 15:26:34 +0900

    Style: 모바일에서는 바 아이콘으로 표시

--- app/(components)/nav/_components/NavItems.tsx
+++ app/(components)/nav/_components/NavItems.tsx
@@ -2,40 +2,49 @@
 
 import Link from "next/link";
 import React from "react";
-import { FaGithub, FaEnvelope } from "react-icons/fa";
+import { FaGithub, FaEnvelope, FaBars } from "react-icons/fa";
 import { navItemList } from "./navItemList";
 import { usePathname } from "next/navigation";
 
 const NavItems = () => {
   const path = usePathname();
   return (
-    <ul className="flex items-center gap-4">
-      {navItemList.map((item) => (
-        <Link key={item.label} href={item.href}>
-          <li
-            className={`hover:animate-navItemHoverEffect ${
-              path === item.href ? "text-tertiary underline" : ""
-            }`}
-          >
-            {item.label}
+    <>
+      <ul className="items-center gap-4 mobile:hidden laptop:flex desktop:flex">
+        {navItemList.map((item) => (
+          <Link key={item.label} href={item.href}>
+            <li
+              className={`hover:animate-navItemHoverEffect ${
+                path === item.href ? "text-tertiary underline" : ""
+              }`}
+            >
+              {item.label}
+            </li>
+          </Link>
+        ))}
+        <Link
+          href={process.env.NEXT_PUBLIC_GITHUB as string}
+          aria-label="깃허브 바로가기"
+          target="_blank"
+        >
+          <li>
+            <FaGithub size={20} />
           </li>
         </Link>
-      ))}
-      <Link
-        href={process.env.NEXT_PUBLIC_GITHUB as string}
-        aria-label="깃허브 바로가기"
-        target="_blank"
-      >
-        <li>
-          <FaGithub size={20} />
-        </li>
-      </Link>
-      <Link href={`mailto:${process.env.NEXT_PUBLIC_EMAIL}`}>
-        <li>
-          <FaEnvelope size={20} />
-        </li>
-      </Link>
-    </ul>
+        <Link href={`mailto:${process.env.NEXT_PUBLIC_EMAIL}`}>
+          <li>
+            <FaEnvelope size={20} />
+          </li>
+        </Link>
+      </ul>
+      <ul className="items-center gap-4 mobile:flex laptop:hidden desktop:hidden">
+        <Link href="/">
+          <li>
+            <FaBars size={20} />
+          </li>
+        </Link>
+      </ul>
+    </>
   );
 };
 
