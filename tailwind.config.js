77e1bba910a0d8e81bba0847bf2ea40737e00751
Hyun-EG <codiee@naver.com>
2025-09-05 21:24:12 +0900

    Chore: 토스트 애니메이션 추가중

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,18 +20,38 @@ module.exports = {
         light: "#ffffff",
       },
       keyframes: {
+        // Nav
         "nav-item-hover-effect": {
           "0%": { color: "#ffffff" },
           "100%": { color: "#ED7474" },
         },
+
+        // 투명도 이펙트
         "opacity-effect": {
           "0%": { opacity: 0 },
           "100%": { opacity: 1 },
+        },
+
+        // Toast
+        "toast-effect": {
+          "0%": {
+            transform: "translateX(100%)",
+            opacity: "0",
+          },
+          "50%": {
+            transform: "translateX(0%)",
+            opacity: "1",
+          },
+          "100%": {
+            transform: "translateX(100%)",
+            opacity: "0",
+          },
         },
       },
       animation: {
         navItemHoverEffect: "nav-item-hover-effect 0.5s ease-in-out forwards",
         opacityEffect: "opacity-effect 0.5s ease-in-out forwards",
+        toastEffect: "toast-effect 2s ease-in-out forwards",
       },
     },
   },
