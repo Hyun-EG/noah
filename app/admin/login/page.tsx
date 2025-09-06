75b7b428297399b594d6c5b93ba706ae81503e28
seonghyun <codiee@naver.com>
2025-09-06 12:00:56 +0900

    Style: 아래 패딩 추가

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -3,7 +3,7 @@ import SignInForm from "./_components/SignInForm";
 
 const SignInPage = () => {
   return (
-    <main className="w-full h-[calc(100vh-3rem)] flex flex-col justify-center items-center gap-10">
+    <main className="w-full h-[calc(100vh-3rem)] pb-20 flex flex-col justify-center items-center gap-10">
       <h1 className="text-center text-2xl">관리자 로그인</h1>
       <SignInForm />
     </main>
