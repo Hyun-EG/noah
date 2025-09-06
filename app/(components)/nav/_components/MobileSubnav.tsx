56e168f8ec02fedf306625580ef3d3fd074e21ba
seonghyun <codiee@naver.com>
2025-09-06 13:29:28 +0900

    Style: 안쓰는 Variants 삭제

--- app/(components)/nav/_components/MobileSubnav.tsx
+++ app/(components)/nav/_components/MobileSubnav.tsx
@@ -1,24 +1,16 @@
 "use client";
 
-import { cva } from "class-variance-authority";
-import clsx from "clsx";
 import React from "react";
 import { navItemList } from "./navItemList";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-const SubnavVariants = cva(
-  "absolute top-12 left-0 z-50 w-full h-[calc(100vh-3rem)] py-10 px-4 animate-opacityEffect bg-secondary dark:bg-dark dark:text-secondary mobile:block laptop:hidden desktop:hidden"
-);
-
-const navItemsVariants = cva("flex flex-col justify-center gap-4");
-
 const MobileSubnav = () => {
   const path = usePathname();
 
   return (
-    <nav className={clsx(SubnavVariants())}>
-      <ul className={clsx(navItemsVariants())}>
+    <nav className="absolute top-12 left-0 z-50 w-full h-[calc(100vh-3rem)] py-10 px-4 animate-opacityEffect bg-secondary dark:bg-dark dark:text-secondary mobile:block laptop:hidden desktop:hidden">
+      <ul className="flex flex-col justify-center gap-4">
         {navItemList.map((item) => (
           <Link href={item.href} key={item.label}>
             <li
