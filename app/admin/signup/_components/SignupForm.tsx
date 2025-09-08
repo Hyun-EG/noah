78a4e11dcfdb258bae7d31a4989ff419cb8618a6
Hyun-EG <codiee@naver.com>
2025-09-08 18:23:47 +0900

    Style: 회원가입 폼 스타일 수정

--- app/admin/signup/_components/SignupForm.tsx
+++ app/admin/signup/_components/SignupForm.tsx
@@ -15,25 +15,25 @@ const SignupForm = () => {
       onSubmit={handleSubmit(onSubmit)}
     >
       <input
-        className="w-full h-12 pl-2 border outline-primary dark:border-light bg-light dark:bg-dark rounded-lg"
+        className="w-full h-12 pl-2 border outline-primary dark:border-light rounded-lg"
         {...register("name", { required: "성함을 입력해주세요." })}
         type="text"
         placeholder="성함을 입력해주세요."
       />
       <input
-        className="w-full h-12 pl-2 border outline-primary dark:border-light bg-light dark:bg-dark rounded-lg"
+        className="w-full h-12 pl-2 border outline-primary dark:border-light rounded-lg"
         {...register("id", { required: "아이디를 입력해주세요." })}
         type="text"
         placeholder="아이디를 입력해주세요."
       />
       <input
-        className="w-full h-12 pl-2 border outline-primary dark:border-light bg-light dark:bg-dark rounded-lg"
+        className="w-full h-12 pl-2 border outline-primary dark:border-light rounded-lg"
         {...register("password", { required: "비밀번호를 입력해주세요." })}
         type="text"
         placeholder="비밀번호를 입력해주세요."
       />
       <input
-        className="w-full h-12 pl-2 border outline-primary dark:border-light bg-light dark:bg-dark rounded-lg"
+        className="w-full h-12 pl-2 border outline-primary dark:border-light rounded-lg"
         {...register("confirmPassword", {
           required: "비밀번호를 한번 더 입력해주세요",
         })}
@@ -41,7 +41,7 @@ const SignupForm = () => {
         placeholder="비밀번호를 한번 더 입력해주세요."
       />
       <button
-        className="w-full h-12 bg-primary text-light rounded-lg"
+        className="w-full h-12 bg-primary border dark:bg-dark text-light rounded-lg"
         type="submit"
       >
         회원가입 하기
