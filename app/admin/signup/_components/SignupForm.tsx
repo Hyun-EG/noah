1c63be337285232d75d555f8af3950a983d2e103
Hyun-EG <codiee@naver.com>
2025-09-13 16:29:53 +0900

    Feat: 회원가입 api 연결 및 로그인 이동

--- app/admin/signup/_components/SignupForm.tsx
+++ app/admin/signup/_components/SignupForm.tsx
@@ -1,24 +1,60 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import React from "react";
 import { useForm } from "react-hook-form";
 
 const SignupForm = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, getValues } = useForm();
+  const router = useRouter();
 
-  const onSubmit = () => {
-    console.log("d");
+  const onSignupSubmit = async () => {
+    const formData = getValues();
+
+    try {
+      const res = await fetch("/api/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userName: formData.name,
+          userEmail: formData.email,
+          userId: formData.id,
+          password: formData.password,
+          confirmPassword: formData.confirmPassword,
+        }),
+      });
+
+      const result = await res.json();
+
+      if (!res.ok) {
+        alert(result.message || "오류가 발생했습니다");
+        return;
+      }
+
+      alert(result.message);
+      router.push("/admin/login");
+    } catch (_) {
+      alert("서버 에러");
+    }
   };
   return (
     <form
       className="w-full laptop:w-[80%] desktop:w-[70%] px-10 pb-20 flex flex-col justify-center items-center gap-2"
-      onSubmit={handleSubmit(onSubmit)}
+      onSubmit={handleSubmit(onSignupSubmit)}
     >
       <input
         className="w-full h-12 pl-2 border outline-primary dark:border-light rounded-lg"
         {...register("name", { required: "성함을 입력해주세요." })}
         type="text"
         placeholder="성함을 입력해주세요."
+      />
+      <input
+        className="w-full h-12 pl-2 border outline-primary dark:border-light rounded-lg"
+        {...register("email", { required: "이메일을 입력해주세요." })}
+        type="email"
+        placeholder="이메일을 입력해주세요."
       />
       <input
         className="w-full h-12 pl-2 border outline-primary dark:border-light rounded-lg"
@@ -29,7 +65,7 @@ const SignupForm = () => {
       <input
         className="w-full h-12 pl-2 border outline-primary dark:border-light rounded-lg"
         {...register("password", { required: "비밀번호를 입력해주세요." })}
-        type="text"
+        type="password"
         placeholder="비밀번호를 입력해주세요."
       />
       <input
@@ -37,7 +73,7 @@ const SignupForm = () => {
         {...register("confirmPassword", {
           required: "비밀번호를 한번 더 입력해주세요",
         })}
-        type="text"
+        type="password"
         placeholder="비밀번호를 한번 더 입력해주세요."
       />
       <button
