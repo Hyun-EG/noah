0a20b819576c764d3ffb4ab382febfa98bb21ba9
seonghyun <codiee@naver.com>
2025-09-14 20:17:16 +0900

    Feat: 컨텐트 데이터 연결

--- app/post/[id]/_components/main/ContentBox.tsx
+++ app/post/[id]/_components/main/ContentBox.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 
-const ContentBox = () => {
+const ContentBox = ({ content }: { content: string }) => {
   return (
     <div className="w-full px-2">
       <h2 className="text-2xl">본문</h2>
+      <div className="py-5">
+        <p dangerouslySetInnerHTML={{ __html: content }} />
+      </div>
     </div>
   );
 };
