b9e447a671e9a209ea0e9327ab8c4ce1b8ae4bcb
seonghyun <codiee@naver.com>
2025-09-14 20:18:11 +0900

    Feat: 타이틀로 해당 데이터 찾고 넘김

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,14 +1,15 @@
 import { connectDB } from "@/lib/connectDB";
-import PostDetail, { PostDataType } from "./_components/PostDetail";
+import { ObjectId } from "mongodb";
+import PostDetail from "./_components/PostDetail";
+import { PostDataType } from "./_components/types";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const PostDetailPage = async (props: any) => {
   console.log(props);
-  //   const client = await connectDB();
-  //   const db = client.db("noah");
-  //   const data = (await db.collection("post").find().toArray()) as PostDataType[];
-  //   console.log(data);
-  return <PostDetail />;
+  const client = await connectDB();
+  const db = client.db("noah");
+  const data = await db.collection("post").findOne({ title: props.params.id });
+  return <PostDetail data={data as PostDataType} />;
 };
 
 export default PostDetailPage;
