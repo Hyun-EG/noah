20d73a4e01e513be204a5c0c5236fafff629e867
seonghyun <codiee@naver.com>
2025-09-14 20:17:49 +0900

    Feat: 게시물 페이지 포스트 데이터 받아오고 프롭스 인계

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import PostController from "./_components/PostController";
 import PostContainer from "./_components/PostContainer";
+import { connectDB } from "@/lib/connectDB";
+import { PostDataType } from "./[id]/_components/types";
 
-const PostPage = () => {
+const PostPage = async () => {
+  const client = await connectDB();
+  const db = client.db("noah");
+  const data = await db.collection("post").find().toArray();
   return (
     <section className="px-2">
       <header>
         <PostController />
       </header>
       <main>
-        <PostContainer />
+        <PostContainer data={data as PostDataType[]} />
       </main>
     </section>
   );
