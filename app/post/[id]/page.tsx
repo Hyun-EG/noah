025902e0300d569057ddfdf72da6bdcd35f6e82d
seonghyun <codiee@naver.com>
2025-09-14 23:36:22 +0900

    Feat: 몽고디비 _id 문자열로 변환

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,16 +1,30 @@
+// app/post/[id]/page.tsx
 import { connectDB } from "@/lib/connectDB";
+import { ObjectId } from "mongodb";
 import PostDetail from "./_components/PostDetail";
 import { PostDataType } from "./_components/types";
-import { ObjectId } from "mongodb";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const PostDetailPage = async (props: any) => {
   const client = await connectDB();
   const db = client.db("noah");
-  const data = await db
-    .collection("post")
-    .findOne({ _id: new ObjectId(props.params.id) });
-  return <PostDetail data={data as PostDataType} />;
+  const data = await db.collection("post").findOne({
+    _id: new ObjectId(props.params.id),
+  });
+
+  const serializedData: PostDataType | null = data
+    ? {
+        _id: data._id.toString(),
+        title: data.title,
+        description: data.description,
+        category: data.category,
+        content: data.content,
+        authorId: data.authorId,
+        createdAt: data.createdAt,
+      }
+    : null;
+
+  return <PostDetail data={serializedData} />;
 };
 
 export default PostDetailPage;
