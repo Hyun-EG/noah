d4438cc32a2467f421310ed99674f1902d1a6b2e
seonghyun <codiee@naver.com>
2025-09-14 20:18:48 +0900

    Feat: 헤더박스 , 컨텐트박스 데이터 인계 및 타입 분리해서 불러오기

--- app/post/[id]/_components/PostDetail.tsx
+++ app/post/[id]/_components/PostDetail.tsx
@@ -3,29 +3,16 @@
 import { ObjectId } from "mongodb";
 import HeaderBox from "./header/HeaderBox";
 import ContentBox from "./main/ContentBox";
+import { PostDataType } from "./types";
 
-export interface PostDataType {
-  _id: ObjectId;
-  title: string;
-  description: string;
-  category: string;
-  content: string;
-  authorId: string;
-  createdAt: string;
-}
-
-interface PostDetailProps {
-  data: PostDataType[];
-}
-
-const PostDetail = () => {
+const PostDetail = ({ data }: { data: PostDataType }) => {
   return (
     <section className="flex flex-col gap-4 px-2 py-10">
       <header>
-        <HeaderBox title="ㅇ" date="d" imgUrl="d" />
+        <HeaderBox title={data.title} date={data.createdAt} />
       </header>
       <main>
-        <ContentBox />
+        <ContentBox content={data.content} />
       </main>
       <footer>댓글/공유하기 기능</footer>
     </section>
