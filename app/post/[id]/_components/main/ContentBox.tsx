222304a428de369907fb6075ee334a34b8cc8717
Hyun-EG <codiee@naver.com>
2025-09-14 19:19:02 +0900

    Feat: 디테일 페이지 본문쪽 컴포넌트 생성

---
+++ app/post/[id]/_components/main/ContentBox.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContentBox = () => {
+  return (
+    <div className="w-full px-2">
+      <h2 className="text-2xl">본문</h2>
+    </div>
+  );
+};
+
+export default ContentBox;
