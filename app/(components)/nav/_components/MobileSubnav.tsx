644f0e3f0afcc8fc9ecb6725c3eaec4a8c3f37a6
Hyun-EG <codiee@naver.com>
2025-09-22 19:04:38 +0900

    Feat: 최적화 전 2.4s for 18.2ms 최적화 후 1.6s for 15.3ms

--- app/(components)/nav/_components/MobileSubnav.tsx
+++ app/(components)/nav/_components/MobileSubnav.tsx
@@ -1,28 +1,22 @@
 "use client";
 
-import React from "react";
+import React, { useCallback, useMemo } from "react";
 import { navItemList } from "./navItemList";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-const MobileSubnav = ({
-  setSubnav,
-}: {
-  setSubnav: (state: boolean) => void;
-}) => {
-  const path = usePathname();
+const MobileSubnav = React.memo(
+  ({ setSubnav }: { setSubnav: (state: boolean) => void }) => {
+    const path = usePathname();
 
-  return (
-    <nav className="absolute top-12 left-0 z-50 w-full h-[calc(100vh-3rem)] py-10 px-4 animate-opacityEffect bg-secondary dark:bg-dark dark:text-secondary mobile:block laptop:hidden desktop:hidden">
-      <ul className="flex flex-col justify-center gap-4">
-        {navItemList.map((item) => (
-          <Link
-            onClick={() => {
-              setSubnav(false);
-            }}
-            href={item.href}
-            key={item.label}
-          >
+    const toggleNav = useCallback(() => {
+      setSubnav(false);
+    }, [setSubnav]);
+
+    const navItems = useMemo(
+      () =>
+        navItemList.map((item) => (
+          <Link onClick={toggleNav} href={item.href} key={item.label}>
             <li
               className={`border-b border-tertiary dark:border-light text-center text-sm leading-10 ${
                 path === item.href ? "text-tertiary" : ""
@@ -31,10 +25,18 @@ const MobileSubnav = ({
               {item.label}
             </li>
           </Link>
-        ))}
-      </ul>
-    </nav>
-  );
-};
+        )),
+      [path, toggleNav]
+    );
+
+    return (
+      <nav className="absolute top-12 left-0 z-50 w-full h-[calc(100vh-3rem)] py-10 px-4 animate-opacityEffect bg-secondary dark:bg-dark dark:text-secondary mobile:block laptop:hidden desktop:hidden">
+        <ul className="flex flex-col justify-center gap-4">{navItems}</ul>
+      </nav>
+    );
+  }
+);
+
+MobileSubnav.displayName = "MobileSubnav";
 
 export default MobileSubnav;
