f889b265fbb49268a9d3d6f2e23b1f42a27f309f
Hyun-EG <codiee@naver.com>
2025-09-22 19:21:28 +0900

    Feat: 이미지 불러오기 전에는 로딩 스피너 렌더

--- app/post/_components/PostContainer.tsx
+++ app/post/_components/PostContainer.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import Image from "next/image";
 import { PostDataProps } from "../[id]/_components/types";
 import Link from "next/link";
+import { useState } from "react";
+import LoadingSpinner from "@/app/(components)/common/LoadingSpinner";
 
 const PostContainer = ({ data }: PostDataProps) => {
+  const [isLoading, setIsLoading] = useState(true);
   return (
     <main className="flex flex-wrap gap-4 justify-center laptop:justify-start desktop:justify-start">
       {data.map((item, index) => (
@@ -15,12 +20,18 @@ const PostContainer = ({ data }: PostDataProps) => {
             href={`/post/${item._id}`}
           >
             <div className="relative w-full h-44 bg-gray-100 rounded-xl overflow-hidden">
+              {isLoading && (
+                <div className="w-full h-full flex justify-center items-center bg-dark">
+                  <LoadingSpinner />
+                </div>
+              )}
               <Image
-                src={item.thumbnail || "/icons/icon-logo-512x512.png"}
+                src={item.thumbnail}
                 alt={item.title}
                 fill
                 className="object-fill"
                 priority={true}
+                onLoad={() => setIsLoading(false)}
               />
               <div className="absolute top-0 right-0 flex items-center justify-center w-16 h-5 px-4 py-3 bg-sky-500 rounded-xl text-white">
                 <p className="text-sm">{item.category}</p>
