b5b7eb9a05297ccb3069a3206bcc72254e0d2738
Hyun-EG <codiee@naver.com>
2025-10-01 21:21:30 +0900

    Add: Nav컴포넌트 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Nav from "./(components)/nav/Nav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,6 +39,7 @@ export default function RootLayout({
         />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Nav />
         {children}
       </body>
     </html>
