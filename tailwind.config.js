d3d556236b6e5b03a14d3ace8b43ee456f54d5c1
Hyun-EG <codiee@naver.com>
2025-10-01 21:21:09 +0900

    Chore: 스크린 태블릿 추가 , px값 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,52 +5,12 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        mobile: "250px",
+        mobile: "320px",
+        tablet: "768px",
         laptop: "1024px",
-        desktop: "1280px",
-      },
-      colors: {
-        //주요 테마 색상
-        primary: "#721D21",
-        secondary: "#FADBC8",
-        tertiary: "#ED7474",
-
-        // 다크모드/라이트모드
-        dark: "#121212",
-        light: "#ffffff",
-
-        // 성공/실패
-        success: "#4BD964",
-        failed: "#FF3B30",
-      },
-      keyframes: {
-        // Nav
-        "nav-item-hover-effect": {
-          "0%": { color: "#ffffff" },
-          "100%": { color: "#ED7474" },
-        },
-
-        // 투명도 이펙트
-        "opacity-effect": {
-          "0%": { opacity: 0 },
-          "100%": { opacity: 1 },
-        },
-      },
-      animation: {
-        navItemHoverEffect: "nav-item-hover-effect 0.5s ease-in-out forwards",
-        opacityEffect: "opacity-effect 0.5s ease-in-out forwards",
-        toastFadeInEffect: "toast-fade-in-effect 0.5s ease-in-out forwards",
-        toastFadeOutEffect: "toast-fade-out-effect 0.5s ease-in-out forwards",
+        desktop: "1440px",
       },
     },
   },
-  plugins: [
-    function ({ addUtilities }) {
-      addUtilities({
-        ".transition-slow-color": {
-          transition: "color 0.5s ease-in-out",
-        },
-      });
-    },
-  ],
+  plugins: [],
 };
